0e8cbd3d78b7e7553a291e37e528476ec0531ac2
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-08-07 18:26:00 -0300

    Rename and move Jewel::Gem::Root class to Jewel::Path

---
+++ lib/jewel/path.rb
@@ -0,0 +1,20 @@
+require 'pathname'
+
+module Jewel
+
+  # A Pathname that allows subdirectory access through method chaining.
+  #
+  # @author Matheus Afonso Martins Moreira
+  # @since 0.1.0
+  class Path < Pathname
+
+    # Joins the name of the missing method and all arguments with this Pathname.
+    #
+    # @return [Jewel::Path] new instance with the results of the join
+    def method_missing(*arguments, &block)
+      self.class.new join(*arguments), &block
+    end
+
+  end
+
+end
